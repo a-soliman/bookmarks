a859f47740b63e637265014c6428b8f6225ce893
a-soliman <ahmed.soliman@programmer.net>
2019-06-02 07:47:21 -0700

    started class App

--- app/client/renderer.js
+++ app/client/renderer.js
@@ -2,3 +2,11 @@ const { shell } = require('electron');
 const parser = require('./parser');
 const storage = require('./storage');
 const ui = require('./ui');
+
+class App {
+  constructor(storage, parser, ui) {
+    this.storage = storage;
+    this.ui = ui;
+    this.parser = parser;
+  }
+}
