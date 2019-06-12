1ab39c37d6f80e0a40b1a05513bb4814bfebe705
a-soliman <ahmed.soliman@programmer.net>
2019-06-12 07:52:19 -0700

    add store method

--- app/client/storage.js
+++ app/client/storage.js
@@ -2,4 +2,9 @@ class Storage {
   constructor() {
     this.STORE = this.get();
   }
+
+  store(title, url) {
+    this.STORE[url] = title;
+    localStorage.setItem('store', JSON.stringify(this.STORE));
+  }
 }
